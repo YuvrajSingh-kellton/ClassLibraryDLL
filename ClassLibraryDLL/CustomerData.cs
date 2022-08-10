85d27537bfd790c70a4d4effc469f1780448dcc8
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-10 09:23:26 +0530

    Initial Commit

---
+++ ClassLibraryDLL/CustomerData.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDLL
+{
+    public class CustomerData
+    {
+        int customerId { get; set; }
+        string customerName { get; set; }
+        string city { get; set; }
+        long mobileNo { get; set; }
+        int age { get; set; }
+        int units { get; set; }
+        int perUnitCost { get; set; } = 10;
+        int totalCost { get; set; }
+        string paidOrNot { get; set; }
+
+        List<CustomerData> myList = new List<CustomerData>();
+        public void StoreInTable()
+        {
+            myList.Add(new CustomerData
+            {
+                customerId = 01,
+                customerName = "Yuvraj Singh",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 02,
+                customerName = "Vikas",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 03,
+                customerName = "Raman",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 04,
+                customerName = "Ashu",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 05,
+                customerName = "Anubhav",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 06,
+                customerName = "Abhinav Singh",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 07,
+                customerName = "Raj",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 08,
+                customerName = "Rohit Kumar",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 09,
+                customerName = "Harsh Parmar",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 10,
+                customerName = "Rahul Singh",
+                city = "Gurugram",
+                mobileNo = 954545861,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+        }
+        public void ShowCustomerDetails(int iD)
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (iD == myList[i].customerId)
+                {
+                    Console.WriteLine(myList[i].customerId + " " + myList[i].customerName + " " + myList[i].city + " " + myList[i].mobileNo + " "
+                        + myList[i].age);
+                    break;
+                }
+            }
+            return;
+        }
+        public void ShowEBill(int iD)
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (iD == myList[i].customerId)
+                {
+                    Console.WriteLine(myList[i].units + " " + myList[i].totalCost);
+                    break;
+                }
+            }
+            return;
+        }
+        public void ShowEBillReport()
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i].customerId + " " + myList[i].customerName + " " + myList[i].city + " " + myList[i].mobileNo + " "
+                        + myList[i].age + myList[i].units + " " + myList[i].totalCost + " " + myList[i].paidOrNot);
+            }
+            return;
+        }
+    }
+
+}
---
+++ ClassLibraryDLL/UseDLL/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryDLL;
+
+// -------------------------------------I have stored information of 10 customers from ID 01 to 10----------------------------------------- //
+
+namespace ClassLibraryDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerData customerDataobj = new CustomerData();
+            customerDataobj.StoreInTable();
+        TOP:
+            Console.WriteLine("1. ShowCustomerDetails");
+            Console.WriteLine("2. ShowEBill");
+            Console.WriteLine("3. ShowEBillReport For all customers");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.Write("Enter Customer Id - ");
+                    customerDataobj.ShowCustomerDetails(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Customer Id - ");
+                    customerDataobj.ShowEBill(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 3:
+                    customerDataobj.ShowEBillReport();
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    goto TOP;
+            }
+        }
+    }
+}
